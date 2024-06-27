789916919f76b749dee8d0e95e2931959a7927d9
alryyan <alryyan50@gmail.com>
2024-06-28 02:50:59 +0400

    finish

--- test.js
+++ test.js
@@ -4,19 +4,27 @@ import bodyParser from 'body-parser'
 import fs from "fs"
 
 const app = express()
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.use(bodyParser.urlencoded({ extended: true,limit: '50mb' }));
 
+const data = ''
 
 app.post('/', function (req, res) {
     console.log('start ')
-    console.log(req.body)
+    
+    let data = req.body.data
+    data = data.slice(data.indexOf('JVB'))
+    console.log(data)
   res.send('Hello World')
 //   console.log(print)
-  const options = {
-      printer: "oscar",
-    };
-//   print.print("assets/sample.pdf",options).then(console.log);
+  // const options = {
+  //     printer: "oscar",
+  //   };
+    fs.writeFile("out.pdf",data, 'base64', function(err) {
+      console.log(err);
+    });
+  print.print("out.pdf").then(console.log);
   // Here is how I get user to download it nicely as PDF bytes instead of base64 string.
 //   res.type('application/pdf');
 //   res.header('Content-Disposition', `attachment; filename="${id}.pdf"`);
