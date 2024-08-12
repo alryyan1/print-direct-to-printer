7feb014569f5d71510b92753c356167e56fcf4e3
alryyan1 <alryyan50@gmail.com>
2024-08-12 21:32:17 +0400

    print direct

---
+++ test.js
@@ -0,0 +1,35 @@
+import  print  from "pdf-to-printer";
+import express from 'express'
+import bodyParser from 'body-parser'
+import fs from "fs"
+
+const app = express()
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true,limit: '50mb' }));
+
+const data = ''
+
+app.post('/', function (req, res) {
+    console.log('start ')
+    
+    let data = req.body.data
+  
+    data = data.slice(data.indexOf('JVB'))
+    console.log('print Direct',req.body.node_direct)
+  res.send('Hello World')
+//   console.log(print)
+  // const options = {
+  //     printer: "oscar",
+  //   };
+    fs.writeFile("out.pdf",data, 'base64', function(err) {
+      console.log(err);
+    });
+  print.print("out.pdf",{printDialog:req.body.node_direct == 0,paperSize:'A6'}).catch((error)=>console.log(error)).then(console.log);
+  // Here is how I get user to download it nicely as PDF bytes instead of base64 string.
+//   res.type('application/pdf');
+//   res.header('Content-Disposition', `attachment; filename="${id}.pdf"`);
+//   res.send(Buffer.from(base64str, 'base64'));
+})
+
+app.listen(4000)
